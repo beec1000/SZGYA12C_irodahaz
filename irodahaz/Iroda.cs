ba87e9bf1399a0bd39ad6cb51c7ff78c143dc89d
beec1000 <beec1000.minecraft@gmail.com>
2024-01-15 14:07:10 +0100

    Add project files.

---
+++ irodahaz/Iroda.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace irodahaz
+{
+    internal class Iroda
+    {
+        public string Id { get; set; }
+        public int KezdetEv { get; set; }
+        public List<int> IrodaLetszam { get; set; }
+
+        public Iroda(string s)
+        { 
+            var v = s.Split(' ');
+            this.Id = v[0];
+            this.KezdetEv = int.Parse(v[1]);
+            this.IrodaLetszam = new List<int>();
+        }
+
+        public void ToString()
+        {
+            Console.WriteLine($"Cég kódja: {this.Id}, Cég kezdeti éve: {this.KezdetEv}, Irodák létszáma: {this.IrodaLetszam}");
+        }
+    }
+}
---
+++ irodahaz/Program.cs
@@ -0,0 +1,18 @@
+﻿using irodahaz;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace irdoahaz
+{
+    class Program
+    {
+        public static main()
+        {
+            var irodak = new List<Iroda>(@"..\..\..\src\irodahaz.txt");
+        }
+        
+    }
+}
