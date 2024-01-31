926a8fe4bc0ece9c11b8a95fc83c82506a19ac2c
beec1000 <beec1000.minecraft@gmail.com>
2024-01-31 20:49:01 +0100

    f11 not working

--- irodahaz/Program.cs
+++ irodahaz/Program.cs
@@ -15,23 +15,21 @@ namespace irodahaz
 
         static int F10(List<Iroda> i) => i.Sum(d => d.IrodaLetszam.Where(dd => dd > 5).Count());
 
-        static IEnumerable<Iroda> F11(List<Iroda> i)
+        static List<string> F11(List<Iroda> i)
         {
-            var x = i.Where(d => d.IrodaLetszam.Contains(0)).ToList();
-            //var f11 = new List<Iroda>();
-            //for (int i = 0; i < x.Count; i++)
-            //{
-            //    if ()
-            //    {
+            var x = i.Where(iroda => iroda.IrodaLetszam.All(szam => szam == 0)).Select(iroda => $"{iroda.Id} {iroda.Sorszam}").ToList();
 
-            //    }
-            //}
             return x;
         }
 
         static double F12(List<Iroda> i) => i.Where(d => d.Id == ("LOGMEIN")).Average(dd => dd.IrodaLetszam.Sum());
 
-        static int F13(List<Iroda> i) => i.Sum(d => d.IrodaLetszam.Count());
+        static int F14(List<Iroda> i) => i.Sum(d => d.IrodaLetszam.Sum());
+
+        static int F15(List<Iroda> i) => i.Min(d => d.KezdetEv);
+
+        static int F16(List<Iroda> i) => DateTime.Now.Year - i.Max(d => d.KezdetEv);
+
 
         public static void Main(string[] args)
         {
@@ -64,31 +62,34 @@ namespace irodahaz
             Console.WriteLine("10. feladat");
             Console.WriteLine($"Ötnél többen {F10(irodak)} irodában vannak.");
 
-            //11. feladat    
+            //11. feladat
             var sw11 = new StreamWriter(@"..\..\..\src\irodahazF11.txt");
             foreach (var i in F11(irodak))
             {
-                sw11.WriteLine($"{i.Id} {i.Sorszam}. Emeleten {string.Join(", ", i.IrodaLetszam.Where(d => d == 0).Count())}. Irodában/Irodákban");
+                sw11.WriteLine($"{i}");
             }
 
             Console.WriteLine("12. feladat");
             Console.WriteLine($"Az átlagos dolgozók száma: {F12(irodak)}");
 
-            //13. feladat
-
-            sw11.WriteLine();
-            //var f13 = irodak.Select(i => i.IrodaLetszam);
-
-            //foreach (var i in irodak)
-            //{
-            //    sw11.WriteLine($"{i.Sorszam}. Emelet | {.Count()} dolgozó");
-            //}
+            // 13. feladat
+            sw11.WriteLine("Emelet | Dolgozók száma");
+            foreach (var i in irodak)
+            {
+                sw11.WriteLine($"{i.Sorszam}. Emelet | {i.IrodaLetszam.Count(dd => dd > 0)} dolgozó");
+            }
 
             sw11.Close();
 
+
             Console.WriteLine("14. feladat");
-            Console.WriteLine($"Az irodaházban összesen {F13(irodak)} ember dolgozik.");
+            Console.WriteLine($"Az irodaházban összesen {F14(irodak)} ember dolgozik.");
+
+            Console.WriteLine("15. feladat");
+            Console.WriteLine($"Az első irodabérlés {F15(irodak)}-ban/ben törtnént.");
+
+            Console.WriteLine("16. feladat");
+            Console.WriteLine($"{F16(irodak)} éve nem történt új irodabérlés.");
         }
-        
     }
 }
